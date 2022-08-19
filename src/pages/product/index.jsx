993930e25eec2ce54cbbd2bae8298cb18f93215e
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2022-08-19 22:27:42 +0700

    convert to react-bootstrap product detail

--- src/pages/product/index.jsx
+++ src/pages/product/index.jsx
@@ -1,21 +1,25 @@
 import Header from "../../components/header";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 const Index = () => {
   return (
     <>
       <Header />
-      <div className="row justify-content-center">
-        <div className="col-md-12 pb-5 text-center">
+      <Row className="row justify-content-center pt-4">
+        <Col className="col-md-12 pb-5 text-center">
           <h1>Product Black Pepper</h1>
-        </div>
-        <div className="col-md-6 text-center">
-          <img
-            src="./images/product-1.png"
+        </Col>
+        <Col className="col-md-6 text-center">
+          <Image
+            src="/images/product-1.png"
             alt="product apple"
             height={400}
             width={400}
           />
-        </div>
-        <div className="col-md-6">
+        </Col>
+        <Col className="col-md-6">
           <h4 className="text-uppercase text-black-50">Rp. 9000</h4>
           <h1 className="display-5">Black Pepper</h1>
           <p className="lead">
@@ -23,9 +27,9 @@ const Index = () => {
             Reprehenderit ut eligendi explicabo vero sint, blanditiis adipisci
             obcaecati vel facere natus.
           </p>
-          <button className="btn btn-warning">Go to cart</button>
-        </div>
-      </div>
+          <Button className="btn btn-warning">Go to cart</Button>
+        </Col>
+      </Row>
     </>
   );
 };
