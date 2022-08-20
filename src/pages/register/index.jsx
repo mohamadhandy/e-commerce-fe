4c9982209579afe660330b5df7f5956466149e7e
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2022-08-20 15:54:34 +0700

    change to reactstrap

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -6,12 +6,7 @@ import {
 } from "../../config/firebase/index";
 import { useNavigate } from "react-router-dom";
 import Header from "../../components/header";
-import { Form } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
-import Image from "react-bootstrap/Image";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import { Form, Input, Container, Row, Col, Button } from "reactstrap";
 
 const Index = () => {
   const [user, loading, error] = useAuthState(auth);
@@ -48,11 +43,14 @@ const Index = () => {
       <Header />
       <section className="vh-100 pt-4">
         <Container className="container h-100">
-          <Row className="row d-flex justify-content-center align-items-center h-100">
-            <Col className="col-lg-12 col-xl-11">
+          <Row
+            xs={12}
+            className="justify-content-center align-items-center h-100"
+          >
+            <Col xs={12}>
               <div className="card text-black">
                 <div className="card-body p-md-5">
-                  <Row className="row justify-content-center">
+                  <Row xs={12}>
                     <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
                       <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
                         Sign up
@@ -61,80 +59,64 @@ const Index = () => {
                         <div className="d-flex flex-row align-items-center mb-4">
                           <i className="bx bx-user me-3"></i>
                           <div className="form-outline flex-fill mb-0">
-                            <Form.Group
-                              className="mb-3"
-                              controlid="formBasicUsername"
-                            >
-                              <Form.Label>Username</Form.Label>
-                              <Form.Control
-                                type="text"
-                                placeholder="Username"
-                                aria-label="Username"
-                                aria-describedby="basic-addon1"
-                                className="form-control"
-                                value={dataRegister.name}
-                                onChange={(e) =>
-                                  setDataRegister({
-                                    ...dataRegister,
-                                    name: e.target.value,
-                                  })
-                                }
-                              />
-                            </Form.Group>
+                            <label>Username</label>
+                            <Input
+                              type="text"
+                              placeholder="Username"
+                              aria-label="Username"
+                              aria-describedby="basic-addon1"
+                              className="form-control"
+                              value={dataRegister.name}
+                              onChange={(e) =>
+                                setDataRegister({
+                                  ...dataRegister,
+                                  name: e.target.value,
+                                })
+                              }
+                            />
                           </div>
                         </div>
                         <div className="d-flex flex-row align-items-center mb-4">
                           <i className="bx bx-envelope fa-lg me-3 fa-fw"></i>
                           <div className="form-outline flex-fill mb-0">
-                            <Form.Group
-                              className="mb-3"
-                              controlId="formBasicEmail"
-                            >
-                              <Form.Label>Email address</Form.Label>
-                              <Form.Control
-                                type="email"
-                                placeholder="Enter email"
-                                className="form-control"
-                                value={dataRegister.email}
-                                onChange={(e) =>
-                                  setDataRegister({
-                                    ...dataRegister,
-                                    email: e.target.value,
-                                  })
-                                }
-                              />
-                            </Form.Group>
+                            <label>Email address</label>
+                            <Input
+                              type="email"
+                              placeholder="Enter email"
+                              className="form-control"
+                              value={dataRegister.email}
+                              onChange={(e) =>
+                                setDataRegister({
+                                  ...dataRegister,
+                                  email: e.target.value,
+                                })
+                              }
+                            />
                           </div>
                         </div>
 
                         <div className="d-flex flex-row align-items-center mb-4">
                           <i className="bx bx-log-in-circle fa-lg me-3 fa-fw"></i>
                           <div className="form-outline flex-fill mb-0">
-                            <Form.Group
-                              className="mb-3"
-                              controlId="formBasicPassword"
-                            >
-                              <Form.Label>Password</Form.Label>
-                              <Form.Control
-                                type="password"
-                                placeholder="Password"
-                                className="form-control"
-                                value={dataRegister.password}
-                                onChange={(e) =>
-                                  setDataRegister({
-                                    ...dataRegister,
-                                    password: e.target.value,
-                                  })
-                                }
-                              />
-                            </Form.Group>
+                            <label>Password</label>
+                            <Input
+                              type="password"
+                              placeholder="Password"
+                              className="form-control"
+                              value={dataRegister.password}
+                              onChange={(e) =>
+                                setDataRegister({
+                                  ...dataRegister,
+                                  password: e.target.value,
+                                })
+                              }
+                            />
                           </div>
                         </div>
 
                         <div className="d-grid gap-2">
                           <Button
-                            variant="primary"
-                            className="btn btn-primary"
+                            color="warning"
                             type="submit"
                             onClick={(e) => handleSubmit(e)}
                             size="lg"
@@ -145,7 +127,7 @@ const Index = () => {
                       </Form>
                     </div>
                     <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-                      <Image
+                      <img
                         src="./images/5334958.jpg"
                         className="img-fluid"
                         alt="Sample"
