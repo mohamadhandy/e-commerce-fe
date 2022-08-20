10ff021f321e0c7d9b636ac5866a3a0e5dd07658
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2022-08-20 21:23:09 +0700

    redesign hero using reactstrap

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,27 +1,87 @@
+import { Container, Row, Col, Button } from "reactstrap";
 const Hero = () => {
   return (
     <>
-      <div className="container pt-5 px-5">
-        <div className="row">
-          <div className="col-md-7">
-            <h5 className="tuttie-fruittie-h1">
-              Tuttie<br></br>Fruittie
-            </h5>
-            <p className="card-text display-5">
-              YOUR BEST FRUIT ON YOUR BEST DAY
-            </p>
-            <button
-              type="button"
-              className="btn btn-warning btn-lg px-4 me-md-2"
+      <Container>
+        <Row xs={12}>
+          <Col xs={12}>
+            <div
+              id="carouselExampleDark"
+              className="carousel carousel-dark slide"
+              data-bs-ride="carousel"
             >
-              Explore Now
-            </button>
-          </div>
-          <div className="col-md-5">
-            <img src="./images/5334958.jpg" width={500} height={400} loading="lazy" alt="" />
-          </div>
-        </div>
-      </div>
+              <div className="carousel-indicators">
+                <button
+                  type="button"
+                  data-bs-target="#carouselExampleDark"
+                  data-bs-slide-to="0"
+                  className="active"
+                  aria-current="true"
+                  aria-label="Slide 1"
+                ></button>
+                <button
+                  type="button"
+                  data-bs-target="#carouselExampleDark"
+                  data-bs-slide-to="1"
+                  aria-label="Slide 2"
+                ></button>
+              </div>
+              <div className="carousel-inner">
+                <div className="carousel-item active" data-bs-interval="10000">
+                  <img
+                    src="/images/5334958.jpg"
+                    className="d-block w-100"
+                    alt="..."
+                  />
+                  <div className="carousel-caption d-none d-md-block">
+                    <h5>We Sell Premium Fruits</h5>
+                    <p>
+                      Hygiene Vegetables from Our Farm, Really Healthy!
+                    </p>
+                  </div>
+                </div>
+                <div className="carousel-item" data-bs-interval="2000">
+                  <img
+                    src="/images/vege.png"
+                    className="d-block w-100"
+                    alt="..."
+                  />
+                  <div className="carousel-caption d-none d-md-block">
+                    <h5>We Sell Premium Vegetables</h5>
+                    <p>
+                      Fruits from our farm are the best according to environment ministry!
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <button
+                className="carousel-control-prev"
+                type="button"
+                data-bs-target="#carouselExampleDark"
+                data-bs-slide="prev"
+              >
+                <span
+                  className="carousel-control-prev-icon"
+                  aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Previous</span>
+              </button>
+              <button
+                className="carousel-control-next"
+                type="button"
+                data-bs-target="#carouselExampleDark"
+                data-bs-slide="next"
+              >
+                <span
+                  className="carousel-control-next-icon"
+                  aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Next</span>
+              </button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
