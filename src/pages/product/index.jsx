f81cd8b91c987adb390ee94ea622067096061fec
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2022-08-20 15:33:13 +0700

    change to reactstrap

--- src/pages/product/index.jsx
+++ src/pages/product/index.jsx
@@ -1,25 +1,22 @@
 import Header from "../../components/header";
-import Image from "react-bootstrap/Image";
-import Button from "react-bootstrap/Button";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import { Row, Col, Button } from "reactstrap";
 const Index = () => {
   return (
     <>
       <Header />
-      <Row className="row justify-content-center pt-4">
-        <Col className="col-md-12 pb-5 text-center">
-          <h1>Product Black Pepper</h1>
+      <Row lg={12} className="justify-content-center pt-4">
+        <Col md={12} xs={12} className="pb-5 text-center">
+          <h1>Product Apple</h1>
         </Col>
-        <Col className="col-md-6 text-center">
-          <Image
+        <Col md={6} xs={12} className="text-center">
+          <img
             src="/images/product-1.png"
             alt="product apple"
             height={400}
             width={400}
           />
         </Col>
-        <Col className="col-md-6">
+        <Col md={6} xs={12} className="text-center">
           <h4 className="text-uppercase text-black-50">Rp. 9000</h4>
           <h1 className="display-5">Black Pepper</h1>
           <p className="lead">
@@ -27,7 +24,7 @@ const Index = () => {
             Reprehenderit ut eligendi explicabo vero sint, blanditiis adipisci
             obcaecati vel facere natus.
           </p>
-          <Button className="btn btn-warning">Go to cart</Button>
+          <Button color="warning">Go to cart</Button>
         </Col>
       </Row>
     </>
