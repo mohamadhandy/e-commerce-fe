c43643d9ae92bf8b782c000ad9e9269100602de2
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2022-08-20 15:48:56 +0700

    change to reactstrap

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -3,11 +3,7 @@ import { auth, logInWithEmailAndPassword } from "../../config/firebase/index";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import { Container, Row, Col, Button, Input } from "reactstrap";
 
 const Index = () => {
   const [dataLogin, setDataLogin] = useState({ email: "", password: "" });
@@ -31,44 +27,43 @@ const Index = () => {
       <Header />
       <section className="vh-100">
         <Container className="container py-5 h-100">
-          <Row className="row d-flex justify-content-center align-items-center h-100">
-            <Col className="col-12 col-md-8 col-lg-6 col-xl-5">
+          <Row
+            lg={12}
+            className="justify-content-center align-items-center h-100"
+          >
+            <Col xs={12}>
               <div className="card shadow-2-strong section-card-login">
                 <div className="card-body p-5 text-center">
                   <h3 className="mb-5">Sign in</h3>
 
                   <div className="form-outline mb-4">
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                      <Form.Label>Email address</Form.Label>
-                      <Form.Control
-                        type="email"
-                        placeholder="Enter email"
-                        className="form-control form-control-lg"
-                        onChange={(e) =>
-                          setDataLogin({ ...dataLogin, email: e.target.value })
-                        }
-                      />
-                    </Form.Group>
+                    <label>Email address</label>
+                    <Input
+                      type="email"
+                      placeholder="Enter email"
+                      className="form-control form-control-lg"
+                      onChange={(e) =>
+                        setDataLogin({ ...dataLogin, email: e.target.value })
+                      }
+                    />
                   </div>
 
                   <div className="form-outline mb-4">
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                      <Form.Label>Password</Form.Label>
-                      <Form.Control
-                        type="password"
-                        placeholder="Password"
-                        className="form-control form-control-lg"
-                        onChange={(e) =>
-                          setDataLogin({
-                            ...dataLogin,
-                            password: e.target.value,
-                          })
-                        }
-                      />
-                    </Form.Group>
+                    <label>Password</label>
+                    <Input
+                      type="password"
+                      placeholder="Password"
+                      className="form-control form-control-lg"
+                      onChange={(e) =>
+                        setDataLogin({
+                          ...dataLogin,
+                          password: e.target.value,
+                        })
+                      }
+                    />
                   </div>
                   <Button
-                    variant="primary"
+                    color="warning"
                     type="submit"
                     onClick={(e) => handleSubmit(e, "login")}
                   >
@@ -76,9 +71,8 @@ const Index = () => {
                   </Button>
                   <hr className="my-4" />
                   <Button
-                    variant="primary"
+                    color="warning"
                     type="submit"
-                    size="lg"
                     onClick={(e) => handleSubmit(e, "login")}
                   >
                     <i className="bx bxl-google"></i> Sign in with google
