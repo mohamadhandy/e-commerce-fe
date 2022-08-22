b74df8140cab9f94ddf56d0a0aa8a33cb49ae2c9
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2022-08-22 13:18:57 +0700

    improve code using async await

--- src/hooks/useFetchFoods.js
+++ src/hooks/useFetchFoods.js
@@ -2,30 +2,13 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export const useFetchFoods = () => {
-  // const [isLoading, setIsLoading] = useState(false);
   const [APIData, setAPIData] = useState([]);
-
-  // const getProducts = async () => {
-  //   try {
-  //     setIsLoading(true);
-  //     const res = await axios.get(`https://6302f81dc6dda4f287c08864.mockapi.io/api/v1/Foods`)
-  //     console.log("response", response)
-  //     setAPIData(res.data);
-  //   } catch (err) {
-  //     console.log(err);
-  //   } finally {
-  //     setIsLoading(false);
-  //   }
-  //   return [isLoading, APIData]
-  // }
-
   useEffect(() => {
-    axios
-      .get(`https://6302f81dc6dda4f287c08864.mockapi.io/api/v1/Foods`)
-      .then((response) => {
-        console.log("respo", response)
-        setAPIData(response.data);
-      });
+    const fetchFoods = async () => {
+      const response = await axios.get(`https://6302f81dc6dda4f287c08864.mockapi.io/api/v1/Foods`)
+      setAPIData(response.data)
+    }
+    fetchFoods()
   }, []);
   return { APIData }
 };
