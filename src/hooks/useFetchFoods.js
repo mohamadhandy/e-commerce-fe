35274aea9329959dc8e3129c6ebaae2d21bdd77e
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2022-08-22 13:22:18 +0700

    improve code using try catch finally

--- src/hooks/useFetchFoods.js
+++ src/hooks/useFetchFoods.js
@@ -2,13 +2,23 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export const useFetchFoods = () => {
+  const [loading, setLoading] = useState(false);
   const [APIData, setAPIData] = useState([]);
   useEffect(() => {
     const fetchFoods = async () => {
-      const response = await axios.get(`https://6302f81dc6dda4f287c08864.mockapi.io/api/v1/Foods`)
-      setAPIData(response.data)
-    }
-    fetchFoods()
+      try {
+        const response = await axios.get(
+          `https://6302f81dc6dda4f287c08864.mockapi.io/api/v1/Foods`
+        );
+        setAPIData(response.data);
+        setLoading(true);
+      } catch (error) {
+        console.log("error", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchFoods();
   }, []);
-  return { APIData }
+  return { APIData, loading };
 };
