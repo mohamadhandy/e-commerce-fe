8b3eb01e03634d1b4bdccec567f6213658a1537f
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2022-08-24 23:14:48 +0700

    redesign product list

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,43 +1,74 @@
 import { Link } from "react-router-dom";
+import { auth } from "../config/firebase/index";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const ListProducts = ({ foods, loading }) => {
+  const [user] = useAuthState(auth);
   return !loading ? (
     <div className="row justify-content-center" id="products">
       {foods.map((product, index) => (
-        <div className="col-lg-4 col-md-12 mb-4" key={index}>
-          <Link
-            to={`/products/${product.id}`}
-            style={{ textDecoration: "none" }}
-          >
-            <div className="height d-flex justify-content-center align-items-center">
-              <div className="card p-3">
-                <div className="d-flex justify-content-between align-items-center ">
-                  <div className="mt-2">
-                    <h4 className="text-uppercase">{product.name}</h4>
-                    <div className="mt-5">
-                      <h1 className="main-heading mt-0">{product.category}</h1>
-                      <div className="d-flex flex-row user-ratings">
-                        <div className="ratings">
-                          <i className="bx bxs-star"></i>
-                          <i className="bx bxs-star"></i>
-                          <i className="bx bxs-star"></i>
-                          <i className="bx bxs-star"></i>
-                          <i className="bx bx-star"></i>
-                        </div>
-                        <h6 className="text-muted ml-1">4/5</h6>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="image">
-                    <img src={product.url} width="200" />
-                  </div>
-                </div>
-                <p>{product.description}</p>
-
-                <button className="btn btn-primary">Add to cart</button>
+        <div className="col-lg-3 col-md-6 col-xs-12 mb-4" key={index}>
+          <div className="product-item justify-content-start">
+            <div className="position-relative bg-light overflow-hidden">
+              <img
+                className="img-fluid w-50 h-50"
+                src={product.url}
+                alt="Product"
+              />
+              <div className="bg-warning rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">
+                New
               </div>
             </div>
-          </Link>
+            <div className="text-center p-4">
+              <span className="d-block h5 mb-2" href="">
+                {product.name}
+              </span>
+              <span className="text-success me-1">
+                Rp. {product.finalPrice}
+              </span>
+              <span className="text-body text-decoration-line-through">
+                Rp. {product.price}
+              </span>
+              <p className="text-body">{product.description}</p>
+            </div>
+            <div className="d-flex border-top">
+              {user ? (
+                <>
+                  <small className="w-50 text-center border-end py-2">
+                    <Link
+                      to={`/products/${product.id}`}
+                      style={{ textDecoration: "none" }}
+                      className="text-body"
+                    >
+                      <i className="bx bx-detail text-success me-2"></i>View
+                      detail
+                    </Link>
+                  </small>
+                  <small className="w-50 text-center py-2">
+                    <Link
+                      to="/cart"
+                      style={{ textDecoration: "none" }}
+                      className="text-body"
+                    >
+                      <i className="bx bx-shopping-bag text-success me-2"></i>
+                      Add to cart
+                    </Link>
+                  </small>
+                </>
+              ) : (
+                <small className="w-100 text-center border-end py-2">
+                  <Link
+                    to={`/products/${product.id}`}
+                    style={{ textDecoration: "none" }}
+                    className="text-body"
+                  >
+                    <i className="bx bx-detail text-success me-2"></i>View
+                    detail
+                  </Link>
+                </small>
+              )}
+            </div>
+          </div>
         </div>
       ))}
     </div>
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,27 +1,58 @@
 import Header from "../../components/header";
 import Hero from "../../components/hero";
 import ListProducts from "../../components/products";
-import { Container } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 import { useState } from "react";
 import { useFetchFoods } from "../../hooks/useFetchFoods";
 
 const Index = () => {
-  const [value, setValue] = useState('All')
+  const [value, setValue] = useState("All");
   let foods = useFetchFoods("", value);
   const handleFilter = (e) => {
-    let val = e.target.textContent
-    setValue(val)
-  }
+    let val = e.target.textContent;
+    setValue(val);
+  };
   return (
     <>
       <Header />
       <Hero />
       <Container>
-        <div className="buttons d-flex justify-content-center pb-5">
-          <button className="btn btn-outline-dark me-2" onClick={(e) => handleFilter(e)}>All</button>
-          <button className="btn btn-outline-dark me-2" onClick={(e) => handleFilter(e)}>Vegetables</button>
-          <button className="btn btn-outline-dark me-2" onClick={(e) => handleFilter(e)}>Fruits</button>
-          <button className="btn btn-outline-dark me-2" onClick={(e) => handleFilter(e)}>Herbs</button>
+        <Row lg={12}>
+          <Col md={12} xs={12}>
+            <div
+              className="section-header mb-5 wow fadeInUp"
+              data-wow-delay="0.1s"
+              style={{ maxWidth: 500 }}
+            >
+              <h1 className="display-5 mb-3">Our Products</h1>
+            </div>
+          </Col>
+        </Row>
+        <div className="buttons d-flex text-start pb-5">
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={(e) => handleFilter(e)}
+          >
+            All
+          </button>
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={(e) => handleFilter(e)}
+          >
+            Vegetables
+          </button>
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={(e) => handleFilter(e)}
+          >
+            Fruits
+          </button>
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={(e) => handleFilter(e)}
+          >
+            Herbs
+          </button>
         </div>
         <ListProducts foods={foods.APIData} loading={foods.loading} />
       </Container>
