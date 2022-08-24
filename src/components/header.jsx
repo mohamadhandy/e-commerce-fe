c5a94106d6a19e6e9745db9b048eec1a2a3df9ed
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2022-08-24 23:14:54 +0700

    remove log

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -31,32 +31,6 @@ const Header = () => {
           </div>
         </nav>
       </div>
-      {/* <nav className="navbar navbar-expand-lg bg-light py-3">
-        <div className="container-fluid">
-          <img src="/images/logo.png" alt="logo" />
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarTogglerDemo02"
-            aria-controls="navbarTogglerDemo02"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item"></li>
-              <li className="nav-item"></li>
-              <li className="nav-item"></li>
-            </ul>
-            <form className="d-flex" role="search">
-              <ButtonHeader />
-            </form>
-          </div>
-        </div>
-      </nav> */}
     </>
   );
 };
